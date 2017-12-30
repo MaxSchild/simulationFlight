ee255b3510675d52381384b18718be8caf550303
Max von Wolff <maxvonwolff@Maxs-MBP-3.fritz.box>
2017-12-30 18:46:19 +0100

    angular velocity

---
+++ workingFiles/angularVel.py
@@ -0,0 +1,13 @@
+# vWinkel
+class angularVel(object):
+	def __init__(self, avX, avY, avZ):
+		self.avX = avX
+		self.avY = avY
+		self.avZ = avZ
+	# addDrehmoment
+	def addTorque(self, t1, t2, t3, I, dt):
+		self.avX += 1 / I * t1 * dt
+		self.avY += 1 / I * t2 * dt
+		self.avZ += 1 / I * t3 * dt
+	def __str__(self):
+		return "avX: " + str(self.avX) + "\n" + "avY: " + str(self.avY) + "\n" + "avZ: " + str(self.avZ)
